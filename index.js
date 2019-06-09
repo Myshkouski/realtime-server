143b6b6b3207d4f7815ca38f830dc105492aed85
Alexei <alexeimyshkouski@gmail.com>
2019-06-09 06:27:13 +0300

    fix error handling

--- index.js
+++ index.js
@@ -10,211 +10,212 @@ const DEFAULT_UPGRADE_STATUS_CODE = 101
 const DEFAULT_WS_CLOSE_CODE = 1008
 
 const DEFAULT_OPTIONS = {
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3,
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
+    perMessageDeflate: {
+        zlibDeflateOptions: {
+            chunkSize: 1024,
+            memLevel: 7,
+            level: 3,
+        },
+        zlibInflateOptions: {
+            chunkSize: 10 * 1024
+        },
 
-    // Other options settable:
-    // Defaults to negotiated value.
-    clientNoContextTakeover: true,
-    // Defaults to negotiated value.
-    serverNoContextTakeover: true,
-    // Defaults to negotiated value.
-    clientMaxWindowBits: 10,
-    // Defaults to negotiated value.
-    serverMaxWindowBits: 10,
+        // Other options settable:
+        // Defaults to negotiated value.
+        clientNoContextTakeover: true,
+        // Defaults to negotiated value.
+        serverNoContextTakeover: true,
+        // Defaults to negotiated value.
+        clientMaxWindowBits: 10,
+        // Defaults to negotiated value.
+        serverMaxWindowBits: 10,
 
-    // Below options specified as default values.
-    // Limits zlib concurrency for perf.
-    concurrencyLimit: 10,
-    // Size (in bytes) below which messages
-    // should not be compressed.
-    threshold: 1024,
-  }
+        // Below options specified as default values.
+        // Limits zlib concurrency for perf.
+        concurrencyLimit: 10,
+        // Size (in bytes) below which messages
+        // should not be compressed.
+        threshold: 1024,
+    }
 }
 
 const FORCED_OPTIONS = {
-  noServer: true
+    noServer: true
 }
 
 function serialize(message) {
-  if (Buffer.isBuffer(message)) {
-    return message
-  }
+    if (Buffer.isBuffer(message)) {
+        return message
+    }
 
-  const typeOfMessage = typeof message
+    const typeOfMessage = typeof message
 
-  if (typeOfMessage === 'string') {
-    return message
-  }
+    if (typeOfMessage === 'string') {
+        return message
+    }
 
-  if (typeOfMessage === 'object') {
-    return JSON.stringify(message)
-  }
+    if (typeOfMessage === 'object') {
+        return JSON.stringify(message)
+    }
 
-  throw new TypeError('Cannot stringify message of type "' + typeOfMessage + '"')
+    throw new TypeError('Cannot stringify message of type "' + typeOfMessage + '"')
 }
 
 async function _handleUpgrade(req, socket, head, extensions, headers) {
-  const ctx = createContext({
-    app: this,
-    req,
-    socket,
-    head,
-    extensions,
-    statusCode: DEFAULT_UPGRADE_STATUS_CODE
-  })
+    const ctx = createContext({
+        app: this,
+        req,
+        socket,
+        head,
+        extensions,
+        statusCode: DEFAULT_UPGRADE_STATUS_CODE
+    })
 
-  await this._composedUpgradeMiddleware(ctx)
+    try {
+        await this._composedUpgradeMiddleware(ctx)
+    } catch (error) {
+        this.emit('error', error)
+    }
 
-  this._wss.completeUpgrade(req, socket, head, extensions, headers)
+    this.completeUpgrade(req, socket, head, extensions, headers)
 
-  return ctx
+    return ctx
 }
 
 async function _handleMessage(websocket, req, socket, head, extensions, headers, message) {
-  const ctx = createContext({
-    app: this,
-    message,
-    websocket,
-    req,
-    socket,
-    statusCode: DEFAULT_WS_CLOSE_CODE,
-    serialize,
-    send(message) {
-      message = this.serialize(message)
-      websocket.send(message)
-    },
-    publish(message) {
-      message = this.serialize(message)
+    const ctx = createContext({
+        app: this,
+        message,
+        websocket,
+        req,
+        socket,
+        statusCode: DEFAULT_WS_CLOSE_CODE,
+        serialize,
+        send(message) {
+            message = this.serialize(message)
+            websocket.send(message)
+        },
+        publish(message) {
+            message = this.serialize(message)
 
-      for (let websocket of this.app._wss.clients) {
-        if (websocket !== this.websocket) {
-          websocket.send(message)
+            for (let websocket of this.app.clients) {
+                if (websocket !== this.websocket) {
+                    websocket.send(message)
+                }
+            }
         }
-      }
+    })
+
+    try {
+        await this._composedMessageMiddleware(ctx)
+    } catch (error) {
+        this.emit('error', error)
     }
-  })
 
-  await this._composedMessageMiddleware(ctx)
+    // if (ctx.statusCode === DEFAULT_WS_STATUS_CODE) {
+    //   ctx.websocket.close(ctx.statusCode, ctx.status)
+    // }
 
-  // if (ctx.statusCode === DEFAULT_WS_STATUS_CODE) {
-  //   ctx.websocket.close(ctx.statusCode, ctx.status)
-  // }
-
-  return ctx
+    return ctx
 }
 
 function _handleError(websocket, error) {
-  console.error(error)
-  websocket.terminate()
+    websocket.terminate()
+    // this.emit('error', error)
 }
 
-class WebsocketServer {
-  constructor(options = {}) {
-    const upgradeMiddleware = []
-    this._upgradeMiddleware = upgradeMiddleware
-    this._composedUpgradeMiddleware = compose(upgradeMiddleware)
-    this._isUpgradeMiddlewareUsed = false
+class WebsocketServer extends WebSocket.Server {
+    constructor(options = {}) {
+        super(Object.assign({}, DEFAULT_OPTIONS, options, FORCED_OPTIONS))
 
-    const messageMiddleware = []
-    this._messageMiddleware = messageMiddleware
-    this._composedMessageMiddleware = compose(messageMiddleware)
+        const upgradeMiddleware = []
+        this._upgradeMiddleware = upgradeMiddleware
+        this._composedUpgradeMiddleware = compose(upgradeMiddleware)
+        this._isUpgradeMiddlewareUsed = false
 
-    const broadcastMiddleware = []
-    this._broadcastMiddleware = broadcastMiddleware
-    this._composedBroadcastMiddleware = compose(broadcastMiddleware)
+        const messageMiddleware = []
+        this._messageMiddleware = messageMiddleware
+        this._composedMessageMiddleware = compose(messageMiddleware)
 
-    const wsServer = new WebSocket.Server(Object.assign({}, DEFAULT_OPTIONS, options, FORCED_OPTIONS))
+        const broadcastMiddleware = []
+        this._broadcastMiddleware = broadcastMiddleware
+        this._composedBroadcastMiddleware = compose(broadcastMiddleware)
 
-    wsServer
-      .on('upgrade', _handleUpgrade.bind(this))
-      .on('connection', (websocket, req, socket, head, extensions, headers) => {
-        websocket
-          .on('message', _handleMessage.bind(this, websocket, req, socket, head, extensions, headers))
-          .once('error', _handleError.bind(websocket))
-      })
-
-    this._wss = wsServer
-  }
-
-  get connected() {
-    return this._wss.clients
-  }
-
-  upgrade(fn) {
-    if (this._isUpgradeMiddlewareUsed) {
-      throw new Error('Upgrade middleware should be used before message middlewares')
+        this
+            .on('upgrade', _handleUpgrade.bind(this))
+            .on('connection', (websocket, req, socket, head, extensions, headers) => {
+                websocket
+                    .on('message', _handleMessage.bind(this, websocket, req, socket, head, extensions, headers))
+                    .once('error', _handleError.bind(this, websocket))
+            })
     }
 
-    const upgradeMiddleware = this._upgradeMiddleware
-    upgradeMiddleware.push(fn.bind(this))
-    this._composedUpgradeMiddleware = compose(upgradeMiddleware)
-
-    debug('defined upgrade middleware')
-
-    return this
-  }
-
-  message(fn) {
-    this._isUpgradeMiddlewareUsed = true
-
-    const _messageMiddleware = this._messageMiddleware
-    _messageMiddleware.push(fn.bind(this))
-    this._composedMessageMiddleware = compose(_messageMiddleware)
-
-    debug('defined message middleware')
-
-    return this
-  }
-
-  broadcast(fn) {
-    const _broadcastMiddleware = this._broadcastMiddleware
-    _broadcastMiddleware.push(fn.bind(this))
-    this._composedBroadcastMiddleware = compose(_broadcastMiddleware)
-
-    debug('defined broadcast middleware')
-
-    return this
-  }
-
-  async publish(message) {
-    const ctx = createContext({
-      // dummy req
-      req: {},
-      app: this,
-      message,
-      serialize,
-      publish(message) {
-        message = this.serialize(message)
-        for (const websocket of this.app._wss.clients) {
-          websocket.send(message)
+    upgrade(fn) {
+        if (this._isUpgradeMiddlewareUsed) {
+            throw new Error('Upgrade middleware should be used before message middlewares')
         }
-      }
-    })
 
-    return await this._composedBroadcastMiddleware(ctx)
-  }
+        const upgradeMiddleware = this._upgradeMiddleware
+        upgradeMiddleware.push(fn.bind(this))
+        this._composedUpgradeMiddleware = compose(upgradeMiddleware)
 
-  callback() {
-    // no callback passed to ws server, instead it emits 'upgrade' event
-    return (req, socket, head) => this._wss.handleUpgrade(req, socket, head /*, cb*/ )
-  }
+        debug('defined upgrade middleware')
 
-  listen() {
-    const server = http.createServer()
-    server.on('upgrade', this.callback())
+        return this
+    }
 
-    server.listen.apply(server, arguments)
+    message(fn) {
+        this._isUpgradeMiddlewareUsed = true
 
-    return server
-  }
+        const _messageMiddleware = this._messageMiddleware
+        _messageMiddleware.push(fn.bind(this))
+        this._composedMessageMiddleware = compose(_messageMiddleware)
+
+        debug('defined message middleware')
+
+        return this
+    }
+
+    broadcast(fn) {
+        const _broadcastMiddleware = this._broadcastMiddleware
+        _broadcastMiddleware.push(fn.bind(this))
+        this._composedBroadcastMiddleware = compose(_broadcastMiddleware)
+
+        debug('defined broadcast middleware')
+
+        return this
+    }
+
+    async publish(message) {
+        const ctx = createContext({
+            app: this,
+            message,
+            serialize,
+            publish(message) {
+                message = this.serialize(message)
+                for (const websocket of this.app.clients) {
+                    websocket.send(message)
+                }
+            }
+        })
+
+        return await this._composedBroadcastMiddleware(ctx)
+    }
+
+    callback() {
+        // no callback passed to ws server, instead it emits 'upgrade' event
+        return this.handleUpgrade.bind(this)
+    }
+
+    listen(...args) {
+        const server = http.createServer()
+
+        server.on('upgrade', this.callback())
+
+        server.listen(...args)
+
+        return server
+    }
 }
 
 module.exports = WebsocketServer
